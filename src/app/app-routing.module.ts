1dfea21e7e4cbefc0cfbf3f22c4a9f51819e3829
Jakob Pilz <20160048@students.htl-perg.ac.at>
2020-01-14 11:18:19 +0100

    Added missing files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home/news', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
-  { path: 'modal', loadChildren: () => import('./LandingPage/NewsPage/modal/modal.module').then( m => m.ModalPageModule) },
-  { path: 'event-modal', loadChildren: () => import('./LandingPage/EventPage/event-modal/event-modal.module').then( m => m.EventModalPageModule) }
+  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)}
 ];
 
 @NgModule({
