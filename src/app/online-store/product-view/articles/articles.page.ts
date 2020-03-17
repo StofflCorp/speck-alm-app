cbc5126ff80f68a46961c1351434b0e4565b1ab6
mluge <20160022@students.htl-perg.ac.at>
2020-03-17 09:28:38 +0100

    Overhand parameter to article view

--- src/app/online-store/product-view/articles/articles.page.ts
+++ src/app/online-store/product-view/articles/articles.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-articles',
@@ -6,10 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./articles.page.scss'],
 })
 export class ArticlesPage implements OnInit {
-
-  constructor() { }
+  data:any;
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    if(this.route.snapshot.data['special']){
+      this.data = this.route.snapshot.data['special'];
+    }
   }
 
 }
--- src/app/online-store/product-view/product-view-routing.module.ts
+++ src/app/online-store/product-view/product-view-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { ProductViewPage } from './product-view.page';
+import {DataResolverService} from "../../resolver/data-resolver.service";
 
 const routes: Routes = [
   {
@@ -10,6 +11,13 @@ const routes: Routes = [
   },
   {
     path: 'articles',
+    loadChildren: () => import('./articles/articles.module').then( m => m.ArticlesPageModule)
+  },
+  {
+    path: 'articles/:id',
+    resolve:{
+      special: DataResolverService
+    },
     loadChildren: () => import('./articles/articles.module').then( m => m.ArticlesPageModule)
   }
 ];
