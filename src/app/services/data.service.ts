643db4ee19b42aa21d125feb21c35d4ffe4355be
mluge <20160022@students.htl-perg.ac.at>
2020-04-22 09:01:14 +0200

    Global Service einbauen

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -9,7 +9,7 @@ import {GlobalService} from "../global.service";
 })
 export class DataService {
   url = "https://speckalm.htl-perg.ac.at/r";
-
+  token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiIiLCJzdWIiOjMsImlhdCI6MTU4NzUzODc1MSwiZXhwIjoxNTg3NTQyMzUxfQ.KL559ziozHecA3q1Cqb11JX6GRP4eLEIH-6AB7kkcS0"
   private data =[];
   constructor(private http: HttpClient, private globalService: GlobalService) { }
   setData(id,data){
@@ -19,15 +19,16 @@ export class DataService {
     return this.data[id];
   }
   searchCategories(title): Observable<any>{
-    if(this.globalService.getId() != null){
+    /*if(this.globalService.getId() != null){
       return this.http.get(`${this.url}/api/${encodeURI(title)}?token=${this.globalService.getToken()}`)
-    }
+    }*/
+    return this.http.get(`${this.url}/api/${encodeURI(title)}?token=${this.token}`)
   }
   searchProducts(title, id): Observable<any>{
-    if(this.globalService.getId() != null){
+    /*if(this.globalService.getId() != null){
       return this.http.get(`${this.url}/api/categories/${encodeURI(id)}/${encodeURI(title)}?token=${this.globalService.getToken()}`)
-    }
-
+    }*/
+    return this.http.get(`${this.url}/api/categories/${encodeURI(id)}/${encodeURI(title)}?token=${this.token}`)
   }
 }
   
