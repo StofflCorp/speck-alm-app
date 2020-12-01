1e0f55a937f304493fd69aed4df9875e2549837e
mluge <20160022@students.htl-perg.ac.at>
2020-12-01 16:09:21 +0100

    Weather

--- src/app/weather/weather-routing.module.ts
+++ src/app/weather/weather-routing.module.ts
@@ -6,7 +6,21 @@ import { WeatherPage } from './weather.page';
 const routes: Routes = [
   {
     path: '',
-    component: WeatherPage
+    component: WeatherPage,
+    children: [
+      {
+        path: '',
+        redirectTo: 'weather'
+      },
+      {
+        path: 'webcam',
+        loadChildren: () => import('./webcam/webcam.module').then( m => m.WebcamPageModule)
+      },
+      {
+        path: 'weather',
+        loadChildren: () => import('./weather/weather.module').then( m => m.WeatherPageModule)
+      }
+    ]
   }
 ];
 
--- src/app/weather/weather.page.ts
+++ src/app/weather/weather.page.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import {map} from "rxjs/operators";
 
 @Component({
   selector: 'app-weather',
@@ -8,28 +6,10 @@ import {map} from "rxjs/operators";
   styleUrls: ['./weather.page.scss'],
 })
 export class WeatherPage{
-  weather;
-  constructor(private http: HttpClient) {
-    this.getWeather().then(data => {
-      this.weather = data;
-    });
-  }
 
+  constructor() { }
 
-  getWeather() {
-    return new Promise(resolve => {
-      this.http.get('http://api.openweathermap.org/data/2.5/onecall?appid=6fa85161c8e45f86ad7115fe57191026&lat=48.253109&lon=14.831470&units=Metric&exclude=minutely,hourly&lang=DE').subscribe(data => {
-        resolve(data);
-      }, err => {
-        console.log(err);
-      });
-    });
-  }
-
-  toDate(weather: any) {
-    let weekdays = ["Sonntag", "Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag"];
-    var date = new Date(weather.dt *1000);
-    return weekdays[date.getDay()];
+  ngOnInit() {
   }
 
 
