0af6b18d0a3d42f1b287fbf1e3bc74868094db4a
mluge <20160022@students.htl-perg.ac.at>
2020-12-22 14:13:31 +0100

    Weather

---
+++ src/app/weather/webcam/webcam.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-webcam',
+  templateUrl: './webcam.page.html',
+  styleUrls: ['./webcam.page.scss'],
+})
+export class WebcamPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
